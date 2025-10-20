b5face10a94633cf6233b6249874b1affff0d537
Bryan_1025561 <1025561@hr.nl>
2025-10-20 14:57:10 +0200

    small update for user experience

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -38,7 +38,11 @@
                     {{ __('Forgot your password?') }}
                 </a>
             @endif
-
+                <a
+                    href="{{ route('register') }}"
+                    class="inline-block px-5 py-1.5 ms-3 dark:text-[#EDEDEC] border-[#19140035] hover:border-[#1915014a] border text-[#1b1b18] dark:border-[#3E3E3A] dark:hover:border-[#62605b] rounded-sm text-sm leading-normal">
+                    Register
+                </a>
             <x-primary-button class="ms-3">
                 {{ __('Log in') }}
             </x-primary-button>
