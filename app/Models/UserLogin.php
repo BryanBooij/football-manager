680b8cb7b0ca540aff2085596c43ccd9f5e6c8b5
Bryan_1025561 <1025561@hr.nl>
2025-11-01 15:03:45 +0100

    added comments function for users to comment after they have logged in at least 5 times deeper validation and added the database tables and connections

--- app/Models/UserLogin.php
+++ app/Models/UserLogin.php
@@ -3,11 +3,9 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
-use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class UserLogin extends Model
 {
-    use HasFactory;
 
     protected $fillable = ['user_id', 'login_count'];
 
